b494bdb33b9604e1966b62f9dc82d0588d0004ee
ducterdev <michel.morales@ducter.com.mx>
2023-01-05 16:52:15 -0600

    VOL-49 :tada: Iniciando proyecto

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('usuario');
+            $table->string('correo')->unique();
+            $table->timestamp('correo_verificado')->nullable();
+            $table->string('contrasena');
+            $table->rememberToken();
+            $table->timestamp('creado');
+            $table->timestamp('actualizado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
