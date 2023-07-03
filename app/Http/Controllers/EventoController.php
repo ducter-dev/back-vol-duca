b6b950c83fb02f182ffa5b872adc9fc24424b5af
ducterdev <michel.morales@ducter.com.mx>
2023-07-03 09:17:04 -0600

    VD-21 Creado CRUD para eventos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -2,84 +2,173 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\EventoResource;
+use App\Models\Bitacora;
 use App\Models\Evento;
+use App\Traits\ApiResponder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EventoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    use ApiResponder;
+
     public function index()
     {
-        //
+        $eventos = Evento::paginate(15);
+
+        $eventos = EventoResource::collection($eventos)->additional([
+            'status' => 'success',
+            "message" => 'Información consultada correctamente.',
+        ]);
+        
+        return $eventos;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        try {
+            $rules = [
+                'descripcion' => 'required|string|max:255',
+            ];
+
+            $validator = Validator::make( $request->all(), $rules, $messages = [
+                'required' => 'El campo :attribute es requerido.',
+                'numeric' => 'El campo :attribute debe ser númerico.',
+                'string' => 'El campo :attribute debe ser tipo texto.',
+                'max' => 'El campo :attribute excede el tamaño requerido (:max).',
+                'date_format' => 'El campo :attribute debe tener formato fecha (Y-m-d) ó formato fecha hora (YYYY-MM-DD HH:mm:ss)',
+            ]);
+
+            if ($validator->fails()) {
+                $errors = $validator->errors()->all();
+                return $this->error("Error al actualizar el registro", $errors);
+            }
+
+            $evento = new Evento($request->all());
+            $evento->save();
+
+            $bitacora = new Bitacora();
+            $bitacora->fecha = date('Y-m-d');
+            $bitacora->fecha_hora = date('Y-m-d H:i:s');
+            $bitacora->evento_id = 1;
+            $bitacora->descripcion1 = 'El usuario ' . $request->user()->usuario;
+            $bitacora->descripcion2 = 'agregó el tipo de evento ' . $evento->id;
+            $bitacora->descripcion3 = '';
+            $bitacora->usuario_id = $request->user()->id;
+            $bitacora->save();
+
+            $resource = new EventoResource($evento);
+
+            return $this->success('Evento registrado correctamente.', [
+                'evento' => $resource
+            ]);
+
+        } catch (\Throwable $th) {
+            return $this->error("Error al registrar el registro, error:{$th->getMessage()}.");
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Evento  $evento
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Evento $evento)
+    public function show($id_evento)
     {
-        //
+        try {
+            $evento = evento::where('id', $id_evento)->first();
+
+            if ($evento == NULL)
+            {
+                return $this->error("Error, NO se encontró el registro.");
+            }
+            
+            $resource = new EventoResource($evento);
+
+            return $this->success('Información consultada correctamente.', [
+                'evento' => $resource
+            ]);
+
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 501);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Evento  $evento
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Evento $evento)
+    public function update(Request $request, $id_evento)
     {
-        //
+        try {
+            $rules = [
+                'descripcion' => 'required|string|max:255',
+            ];
+
+            $validator = Validator::make( $request->all(), $rules, $messages = [
+                'required' => 'El campo :attribute es requerido.',
+                'numeric' => 'El campo :attribute debe ser númerico.',
+                'string' => 'El campo :attribute debe ser tipo texto.',
+                'max' => 'El campo :attribute excede el tamaño requerido (:max).',
+                'date_format' => 'El campo :attribute debe tener formato fecha (Y-m-d) ó formato fecha hora (YYYY-MM-DD HH:mm:ss)',
+            ]);
+
+            if ($validator->fails()) {
+                $errors = $validator->errors()->all();
+                return $this->error("Error al actualizar el registro", $errors);
+            }
+
+            $evento = evento::where('id', $id_evento)->first();
+            
+            if ($evento == NULL)
+            {
+                return $this->error("Error, NO se encontró el registro.");
+            }
+            
+            $evento->descripcion = $request->descripcion;
+            $evento->save();
+
+            $cambios = '';
+            foreach ($evento->getChanges() as $key => $value) {
+                $cambios .= $key . ' - ' . $value . ' | ';
+            }
+
+            $bitacora = new Bitacora();
+            $bitacora->fecha = date('Y-m-d');
+            $bitacora->fecha_hora = date('Y-m-d H:i:s');
+            $bitacora->evento_id = 1;
+            $bitacora->descripcion1 = 'El usuario ' . $request->user()->usuario;
+            $bitacora->descripcion2 = 'modificó el tipo de evento ' . $evento->id;
+            $bitacora->descripcion3 = $cambios;
+            $bitacora->usuario_id = $request->user()->id;
+            $bitacora->save();
+
+            $resource = new EventoResource($evento);
+
+            return $this->success('Registro actualizado correctamente.', [
+                'evento' => $resource
+            ]);
+        } catch (\Throwable $th) {
+            return $this->error("Error al actualizar el registro, error:{$th->getMessage()}.");
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Evento  $evento
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Evento $evento)
+    public function destroy(Request $request, $id_evento)
     {
-        //
-    }
+        try {
+            $evento = evento::where('id', $id_evento)->first();
+            $evento->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Evento  $evento
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Evento $evento)
-    {
-        //
+            $bitacora = new Bitacora();
+            $bitacora->fecha = date('Y-m-d');
+            $bitacora->fecha_hora = date('Y-m-d H:i:s');
+            $bitacora->evento_id = 1;
+            $bitacora->descripcion1 = 'El usuario ' . $request->user()->usuario;
+            $bitacora->descripcion2 = 'eliminó el tipo de evento ' . $evento->id;
+            $bitacora->descripcion3 = '';
+            $bitacora->usuario_id = $request->user()->id;
+            $bitacora->save();
+
+            $resource = new EventoResource($evento);
+
+            return $this->success('Registro borrado correctamente.', [
+                'evento' => $resource
+            ]);
+
+        } catch (\Throwable $th) {
+            return $this->error("Error al eliminar el registro, error:{$th->getMessage()}.");
+        }
     }
 }
